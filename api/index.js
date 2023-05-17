ce7f6d952400698055df681d3488f14b3eee75d4
Camilop4 <jcrp_2005@hotmail.com>
2023-05-17 17:46:51 -0500

    commit17

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["*"],
-    credentials: true,
+    origin: ["http://localhost:3000", "https://foodlandmarket.vercel.app", "https://foodland-76lq3j1fm-camilop4.vercel.app"],
+    credentials: false,
   })
 );
 
